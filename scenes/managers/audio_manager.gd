bc9d155888469a0bffe9a94dd0c1e8195f3c6559
Lee Hull <lhull@exzeo.com>
2024-12-04 18:30:09 -0500

    changes

--- scenes/managers/audio_manager.gd
+++
@@ -1,7 +0,0 @@
-extends Node2D
-class_name AudioManager
-
-@onready var fairy_hit: AudioStreamPlayer2D = $FairyHit
-@onready var fairy_flap: AudioStreamPlayer2D = $FairyFlap
-@onready var score: AudioStreamPlayer2D = $Score
-@onready var background: AudioStreamPlayer2D = $Background
