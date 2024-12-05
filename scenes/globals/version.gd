46b8050ce1118680bdfcc566e95b40eeadf1b4e8
Lee Hull <lhull@exzeo.com>
2024-12-04 23:53:23 -0500

    changes

--- scenes/globals/version.gd
+++ scenes/globals/version.gd
@@ -3,8 +3,4 @@ extends Node
 var version: String = "unknown"
 
 func _ready():
-	var file = FileAccess.open("res://build.txt", FileAccess.READ)
-	if file:
-		version = file.get_as_text()
-		file.close()
 	print("Build Date: ", version)
